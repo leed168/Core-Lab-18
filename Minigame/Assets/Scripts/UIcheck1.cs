ae0629718df236226dde3295957e65f3be072ac7
Jhun Lee <BeingMeltdown@fg2042nonver231.wireless.newschool.edu>
2018-09-25 15:49:32 -0400

    092518

---
+++ Minigame/Assets/Scripts/UIcheck1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIcheck1 : MonoBehaviour
+{
+    [SerializeField] private RawImage check1Image;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            check1Image.enabled = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            check1Image.enabled = false;
+        }
+    }
+}
---
+++ Minigame/Assets/Scripts/UIcheck2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIcheck2 : MonoBehaviour
+{
+    [SerializeField] private RawImage check2Image;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            check2Image.enabled = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            check2Image.enabled = false;
+        }
+    }
+}
