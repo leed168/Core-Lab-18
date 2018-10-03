41d3a5af32a58b90b183eb64e765f47114b799a7
Jhun Lee <Lechernt@FG2042nonVer231.lan1>
2018-10-03 19:57:09 -0400

    100318

---
+++ Minigame/Assets/Scripts/gameControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameControl : MonoBehaviour
+{
+    public static gameControl control;
+
+    public float health;
+
+    private void Awake()
+    {
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(500, 500, 1000, 1000), "Bonus: " + health);
+    }
+
+}
