e70a8d739cfed9e5e0909ec98a941fd4244edca8
Jhun Lee <Lechernt@fg2042nonver231.wireless.newschool.edu>
2018-10-18 17:07:30 -0400

    101818

---
+++ FlappyBird/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed;
+
+    private Vector2 direction;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        GetInput();
+        Move();
+        // tried to make camera stop when dead
+        //if (GameObject.FindGameObjectWithTag("Player") == null)
+        //{
+        //    direction = Vector2.zero;
+        //}
+    }
+
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    private void GetInput()
+    {
+
+        direction = Vector2.right;
+
+    }
+
+}
---
+++ FlappyBird/Assets/Scripts/FlappyMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyMove : MonoBehaviour 
+{
+
+    [SerializeField]
+    private float speed;
+
+    private Vector2 direction;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        GetInput();
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    private void GetInput()
+    {
+
+        direction = Vector2.right;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            direction += Vector2.up;
+        }
+        else
+        {
+            direction += Vector2.down;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(GameObject.FindWithTag("Player"));
+    }
+
+}
